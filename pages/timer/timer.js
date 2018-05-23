3ea2a7f5eb7ba6b1dfea27707f3bda7ec33a01ef
Wenqian1228 <1155077141@link.cuhk.edu.hk>
2018-05-23 18:31:48 +0800

    timer edited roughly

--- pages/timer/timer.js
+++ pages/timer/timer.js
@@ -7,7 +7,7 @@ var wxTimer_bxsc = new timer({
 var reset_bxsc=1;
 
 var wxTimer_bc = new timer({
-  beginTime: "00:01:10",
+  beginTime: "00:01:00",
   name: 'wxTimer_bc',
 })
 var reset_bc = 1;
@@ -37,9 +37,11 @@ Page({
   onLoad: function (options) {
 
     showView_bxsc: (options.showView_bxsc == "false" ? false : true)
-    
+    counting_bxsc: (options.counting_bxsc == "false" ? false : true)
     showView_bc: (options.showView_bc == "false" ? false : true)
+    counting_bc: (options.counting_bxsc == "false" ? false : true)
     showView_dbc: (options.showView_dbc == "false" ? false : true)
+    counting_dbc: (options.counting_bxsc == "false" ? false : true)
   },
 
 
@@ -52,21 +54,51 @@ Page({
     })
     if (!that.data.showView_bxsc){
       wxTimer_bxsc.stop(this);
-     
+      
+      that.setData({
+
+        counting_bxsc: false
+
+      })
       reset_bxsc = 1;
     }
   },
   onChangeShowState_bc: function () {
     var that = this;
     that.setData({
+
       showView_bc: (!that.data.showView_bc)
+
     })
+    if (!that.data.showView_bc) {
+      wxTimer_bc.stop(this);
+
+      that.setData({
+
+        counting_bc: false
+
+      })
+      reset_bc = 1;
+    }
   },
+ 
   onChangeShowState_dbc: function () {
     var that = this;
     that.setData({
+
       showView_dbc: (!that.data.showView_dbc)
+
     })
+    if (!that.data.showView_dbc) {
+      wxTimer_dbc.stop(this);
+
+      that.setData({
+
+        counting_dbc: false
+
+      })
+      reset_dbc = 1;
+    }
   },
 
   /**
@@ -121,47 +153,66 @@ Page({
   
 //包心生菜
   onRequest_bxsc: function(){
-    
+    var that = this;
     if (reset_bxsc==1){
       wxTimer_bxsc.start(this);
-
+      
+      that.setData({
+        counting_bxsc: (!that.data.counting_bxsc)
+      })
 
      reset_bxsc=0;
      
    }
     else if (reset_bxsc==0){
       wxTimer_bxsc.stop(this);
- 
+      that.setData({
+        counting_bxsc: (!that.data.counting_bxsc)
+      })
       reset_bxsc=1;
    }  
   },
 
   //菠菜
   onRequest_bc: function () {
+    var that = this;
     if (reset_bc == 1) {
       wxTimer_bc.start(this);
+
+      that.setData({
+        counting_bc: (!that.data.counting_bc)
+      })
 
       reset_bc = 0;
 
     }
     else if (reset_bc == 0) {
       wxTimer_bc.stop(this);
-
+      that.setData({
+        counting_bc: (!that.data.counting_bc)
+      })
       reset_bc = 1;
     }
   },
 
   //大白菜
   onRequest_dbc: function () {
+    var that = this;
     if (reset_dbc == 1) {
       wxTimer_dbc.start(this);
+
+      that.setData({
+        counting_dbc: (!that.data.counting_dbc)
+      })
 
       reset_dbc = 0;
 
     }
     else if (reset_dbc == 0) {
       wxTimer_dbc.stop(this);
-
+      that.setData({
+        counting_dbc: (!that.data.counting_dbc)
+      })
       reset_dbc = 1;
     }
   },
